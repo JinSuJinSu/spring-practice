57efd8a1288e4b3fc012ab6bbaf969e84e6d9f9d
JinsuJinsu <hjs928@naver.com>
2022-02-09 19:30:53 +0900

    refresh git data for confilction solve

--- fileupload/src/main/java/com/poscoict/fileupload/service/FileUploadService.java
+++ fileupload/src/main/java/com/poscoict/fileupload/service/FileUploadService.java
@@ -10,55 +10,52 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Service
 public class FileUploadService {
+	
 	private static String SAVE_PATH = "/upload-images";
 	private static String URL_BASE = "/images";
 	
-	public String restore(MultipartFile multipartFile) {
-		String url = null;
+	public String restore(MultipartFile multipartFile){
+		String url=null;
 		
 		try {
-			if(multipartFile.isEmpty()) {
-				return url;
-			}
-			
-			String originFilename = multipartFile.getOriginalFilename();
-			String extName = originFilename.substring(originFilename.lastIndexOf('.')+1);
-			String saveFilename = genearteSaveFilename(extName);
-			long fileSize = multipartFile.getSize();
-			
-			System.out.println("##############" + originFilename);
-			System.out.println("##############" + saveFilename);
-			System.out.println("##############" + fileSize);
-			
-			byte[] data = multipartFile.getBytes();
-			OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFilename);
-			os.write(data);
-			os.close();
-			
-			url = URL_BASE + "/" + saveFilename;
-			
-		} catch(IOException ex) {
-			throw new RuntimeException("file upload error:" + ex);
+		if(multipartFile.isEmpty()) {
+			return url;
 		}
-		
+		String originalFilename = multipartFile.getOriginalFilename();
+		String endName = originalFilename.substring(originalFilename.indexOf('.')+1);
+		String saveFileName = generateSaveFileName(endName);
+		long fileSize = multipartFile.getSize();
+		System.out.println("##################" + originalFilename);
+		System.out.println("##################" + fileSize);
+		System.out.println("##################" + saveFileName);
+		System.out.println("##################" + endName);
+		byte[] data = multipartFile.getBytes();
+		OutputStream os = new FileOutputStream(SAVE_PATH + "/" + saveFileName);
+		os.write(data);
+		os.close();
+		url = URL_BASE + "/" + saveFileName;
+;		} 
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException("file upload error : " + e);
+		}
+		System.out.println(url);
 		return url;
+		
 	}
-
-	private String genearteSaveFilename(String extName) {
+	
+	public String generateSaveFileName(String endName) {
 		String filename = "";
-		
 		Calendar calendar = Calendar.getInstance();
-		
-		filename += calendar.get(Calendar.YEAR);
-		filename += calendar.get(Calendar.MONTH);
-		filename += calendar.get(Calendar.DATE);
-		filename += calendar.get(Calendar.HOUR);
-		filename += calendar.get(Calendar.MINUTE);
-		filename += calendar.get(Calendar.SECOND);
-		filename += calendar.get(Calendar.MILLISECOND);
-		filename += ("." + extName);
+		filename+=calendar.get(Calendar.YEAR);
+		filename+=(calendar.get(Calendar.MONTH)+1);
+		filename+=calendar.get(Calendar.DATE);
+		filename+=calendar.get(Calendar.MINUTE);
+		filename+=calendar.get(Calendar.SECOND);
+		filename+=calendar.get(Calendar.MILLISECOND);
+		filename+=("." + endName);
 		
 		return filename;
+		
 	}
-
 }
